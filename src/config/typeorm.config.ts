7be5890ddba4447a2b14a9c7a9c2808b9d3f9743
X147107 <ala.atrash-ext@socgen.com>
2022-02-10 20:37:25 +0100

    config database

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+  schema: process.env.DB_SCHAME,
+  autoLoadEntities: false,
+  migrationsTableName: 'migrations',
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    migrationsDir: 'dist/migrations/*.js',
+  },
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: false,
+  dropSchema: false,
+  migrationsRun: true,
+  logging: true,
+};
+
+export default typeOrmConfig;
---
+++ src/config/typeorm.local.config.ts
@@ -0,0 +1,8 @@
+export const localTypeormConfig = {
+  DATABASE_HOST: 'localhost',
+  DATABASE_PORT: 5432,
+  DATABASE_USER: 'root',
+  DATABASE_PASSWORD: 'root',
+  DATABASE_DB: 'hubservice',
+  DATABASE_SCHEMA: 'public',
+};
