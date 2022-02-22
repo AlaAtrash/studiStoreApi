c13e3691688e62d7804b945b4f6e69cd3978e2d9
X147107 <ala.atrash-ext@socgen.com>
2022-02-22 18:18:40 +0100

    query db

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): Record<string,any> {
+    const result={
+      text:'Hello World!'
+    };
+    return result
   }
 }
